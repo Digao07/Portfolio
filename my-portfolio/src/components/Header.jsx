ac94545a4d5ca0ff65b27d068ab65f47517e8155
Digao <126627525+Digao07@users.noreply.github.com>
2024-10-04 14:38:45 -0300

    refactor: Alinhando e organizando o codigo

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,8 +1,10 @@
 const Header = () => {
     return (
-      <header className="bg-blue-600 p-6 text-white">
+      <header className="bg-blue-600 p-6 text-white" style={{margin: 2}}>
         <h1 className="text-4xl font-bold">Rodrigo Barros</h1>
-        <p className="mt-2 text-lg">Desenvolvedor Frontend Junior</p>
+        <p className="mt-2 text-lg" style={{ display: "flex", justifyContent: "center" }}>
+          Desenvolvedor Frontend Junior
+        </p>
       </header>
     );
   };
--- my-portfolio/src/components/SocialLinks.jsx
+++ my-portfolio/src/components/SocialLinks.jsx
@@ -2,31 +2,31 @@ import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
 
 const SocialLinks = () => {
   return (
-    <nav className="flex items-center justify-between py-6">
+    <nav className="flex items-center justify-between py-6" style={{ display: "flex", justifyContent: "center" }}>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <a href="https://www.linkedin.com/in/rodrigo-barros07/"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="LinkedIn">
-                    <FaLinkedin className="text-4xl"/>
+                    <FaLinkedin size={40} className="text-4xl"/>
             </ a>
             <a href="https://github.com/Digao07"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="GitHub">
-                    <FaGithub className="text-4xl"/>
+                    <FaGithub size={40} className="text-4xl"/>
             </ a>
             <a href="https://www.instagram.com/digaao07/"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Instagram">
-                    <FaInstagram className="text-4xl"/>
+                    <FaInstagram size={40} className="text-4xl"/>
             </ a>
             <a href="https://wa.me/5562998402480"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="WhattsApp">
-                    <FaWhatsapp className="text-4xl"/>
+                    <FaWhatsapp size={40} className="text-4xl"/>
             </ a>
         </div>
     </nav>
